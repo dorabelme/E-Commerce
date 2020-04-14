ac568772dd2752a8e3f561fccd6e9e38113c0c5c
Dora Belme <contact.dorabelme@gmail.com>
2020-04-14 19:17:50 -0400

    user authentication

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
+
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+    keys: ['ghgfbhdfgsdfgsdsbnf']
+}));
 
 app.get('/', (req, res) => {
     res.send(`
     <div>
+    Your id is: ${req.session.userId}
       <form method="POST">
         <input name="email" placeholder="email" />
         <input name="password" placeholder="password" />
@@ -36,9 +44,26 @@ app.get('/', (req, res) => {
 // };
 
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('Account created.')
+app.post('/', async (req, res) => {
+    const { email, password, passwordConfirmation } = req.body;
+    const existingUser = await usersRepo.getOneBy({ email });
+
+    if (existingUser) {
+        return res.send('Email in use.');
+    }
+
+    if (password !== passwordConfirmation) {
+        return res.send('Passwords must match!');
+    }
+
+    // Create a user in our user repo to represent this person
+    const user = await usersRepo.create({ email, password });
+
+    // Store the id of that user inside the users cookie
+    req.session.userId = user.id // Added by cookie session
+
+
+    res.send('Account created!');
 })
 
 
